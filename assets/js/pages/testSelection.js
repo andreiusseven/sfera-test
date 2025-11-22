dc4648ba964fb2400e2d4605d88ab43a72de0aae
andreiusseven <andreius.seven@gmail.com>
2025-11-22 20:29:21 +0100

    Update testSelection.js

--- assets/js/pages/testSelection.js
+++ assets/js/pages/testSelection.js
@@ -1,8 +1,9 @@
-// Gestione pulsanti della pagina di selezione// Gestione pulsanti della pagina di selezione
+// Gestione pulsanti della pagina di selezione
 const fisiatriaButton = document.getElementById("fisiatriaButton");
 const animazioneButton = document.getElementById("animazioneButton");
 const parametriButton = document.getElementById("parametriButton");
 const igieneButton = document.getElementById("igieneButton");
+const geriatriaButton = document.getElementById("geriatriaButton");
 const exitButton = document.getElementById("exitButton");
 
 fisiatriaButton.addEventListener("click", () => {
@@ -22,8 +23,11 @@ igieneButton.addEventListener("click", () => {
   window.location.href = "test.html?topic=igiene";
 });
 
+geriatriaButton.addEventListener("click", () => {
+  window.location.href = "test.html?topic=geriatria";
+});
+
 exitButton.addEventListener("click", () => {
   // Nel web non si chiude il browser: torniamo alla home
   window.location.href = "index.html";
 });
-
